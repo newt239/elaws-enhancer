2c1d086c8ce530058882a143d488a8a1f2d746f0
newt239 <ysti2019.win2020.st@gmail.com>
2024-03-21 16:52:31 +0900

    feat: add convert button

--- popup/index.tsx
+++ popup/index.tsx
@@ -1,24 +1,16 @@
 import { useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [checked, setChecked] = useState<boolean>(false)
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div>
+      <p>
+        Toggle the switch to convert the text from old to new
+      </p>
+      <input type="checkbox" onChange={(e) => {
+        setChecked(true)
+      }} checked={checked} />
     </div>
   )
 }
